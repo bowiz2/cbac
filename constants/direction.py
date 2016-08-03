8e6c5cf27f8e5e3b76050e6706d0d2ba79182674
Nicodemes <sasha@paticon.com>
2016-08-03 20:26:59 +0300

    add vectors

--- constants/direction.py
+++ constants/direction.py
@@ -1,6 +1,18 @@
+from utils import Vector
+
+
 UP = 'up'
 DOWN = 'down'
 NORTH = 'north'
 EAST = 'east'
 SOUTH = 'south'
 WEST = 'west'
+
+vectors = {
+    UP: Vector(0, 1, 0),
+    DOWN: Vector(0, -1, 0),
+    NORTH: Vector(0, 0, 1),
+    EAST: Vector(1, 0, 0),
+    SOUTH: Vector(0, 0, -1),
+    WEST: Vector(-1, 0, 0)
+}
