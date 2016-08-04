3f7cecf83e30b4d29d8cb434b7956446111fa4cd
Nicodemes <sasha@paticon.com>
2016-08-04 12:35:54 +0300

    add the buffer size feature for the constant

--- compound.py
+++ compound.py
@@ -3,7 +3,7 @@ from constants import cb_action, block_id
 
 
 class Compound(object):
-    """Contains the blocks which are part of the compound."""
+    """Contains the blocks which are part of the compound. Later can be bound to a blockspace."""
     def __init__(self, blocks, isolated=False):
         self.blocks = blocks
         self.isolated = isolated
@@ -28,7 +28,14 @@ class CBA(Compound):
 
 
 class Constant(Compound):
-    def __init__(self, number):
+    """
+    When compiled, holds the binary representation of a number.
+    """
+    def __init__(self, number, buffer_size=None):
+        """
+        :param number: The constat value you want to store in the world
+        :param buffer_size: the number of the blocks will be completed to this size if exits.
+        """
         super(Constant, self).__init__(list(), isolated=False)
 
         self.number = number
@@ -42,8 +49,14 @@ class Constant(Compound):
 
             self.blocks.append(Block(material))
 
+        if buffer_size is not None:
+            for i in xrange(buffer_size - len(self.bits)):
+                self.blocks.append(Block(block_id.EMPTY_BLOCK))
 
 class Memory(Compound):
+    """
+    An array of empty blocks which later will be used to store some data.
+    """
     def __init__(self, size, isolated=False):
         """
         :param size: Size of the memory in bits.
