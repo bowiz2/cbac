0a09fa846d0ec97a85b24d757a05b00e0ed3ab5b
Nicodemes <sasha@paticon.com>
2016-08-13 21:02:48 +0300

    fix imports

--- command_shell/__init__.py
+++ command_shell/__init__.py
@@ -1,10 +1,10 @@
-import decorator
+from .command_suspender import CommandSuspender
+from . import decorator
+from .shell_context import ShellContext
+from .command_shell_base import CommandShell
+from .location_shell import LocationShell, BlockShell
+from .compound_shell import CompoundShell
 from .cb_shell import CommandBlockShell
 from .cba_shell import CBAShell
-from .command_shell_base import CommandShell
-from .command_suspender import CommandSuspender
-from .compound_shell import CompoundShell
-from .location_shell import LocationShell, BlockShell
 from .memory_shell import MemoryShell
-from .shell_context import ShellContext
 
