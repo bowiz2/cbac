36bcf0c6032ffb1605bf871072837beec5cd45ad
Nicodemes <sasha@paticon.com>
2016-08-16 22:15:36 +0300

    Add packer test

---
+++ tests/test_packer.py
@@ -0,0 +1,27 @@
+"""Test the packer module"""
+from unittest import TestCase
+
+
+class TestPacker(TestCase):
+    """
+    Test the packet functionality.
+    """
+    def test_pack(self):
+        pass
+
+    def test_pack_areas(self):
+        pass
+
+
+class TestArea(TestCase):
+    """
+    Test the area class.
+    """
+    def test_init(self):
+        pass
+
+    def test_pack(self):
+        pass
+
+    def test_dimensions(self):
+        pass
