1655831c521d12a73ae8cc7019006edb37ad3d15
Nicodemes <sasha@paticon.com>
2016-08-16 21:58:20 +0300

    outline

--- blockspace/packer.py
+++ blockspace/packer.py
@@ -2,6 +2,9 @@ from compound import CBA
 from constants import direction
 from constants.block_id import ISOLATORS
 from utils import Location
+from collections import namedtuple
+
+BlockAssignment = namedtuple('BlockAssignment', ['location', 'direction'])
 
 
 class PackingError(BaseException):
@@ -10,56 +13,43 @@ class PackingError(BaseException):
     """
     pass
 
+class Area(object):
+    def __init__(self, compound):
+        self.compound = compound
 
-def location_gen(compound, blockspace):
-    for x in xrange(blockspace.size[0]):
-        for y in xrange(blockspace.size[1]):
-            for z in xrange(blockspace.size[2]):
-                yield Location(x, y, z)
+    def pack(self):
+        """
+        :return: list of relative Block Assignments.
+        """
+        pass
 
+    def get_dimensions(self):
+        """
+        :return: Get the width, height and length of the area.
+        """
+        pass
 
-def pack(compound, blockspace, spec_location, start_build_direction):
+def pack(compounds, blockspace):
     """
-    Try to pack a compound at a location in a blockspace
+    Takes a collection of compounds and packs them into the blockspace.
+    :param compounds:
+    :param blockspace:
+    :return: list of block assignments.
     """
-    def _try_pack(try_location):
-        current_build_direction = start_build_direction
-        # For each block try to assign location and hope for the location to match.
-        for i, block in enumerate(compound.blocks):
-            # check if this block allready have been assigned location.
-            if block in blockspace.blocks:
-                yield (block, (blockspace.blocks[block], blockspace.directions[block]))
-            else:
-                # assign new location.
-                # try to assign location.
-                direction_vector = direction.vectors[current_build_direction]
+    areas = [Area(compound) for compound in compounds]
+    area_assignments = pack_areas(areas, blockspace)
 
-                possible_location = try_location + (direction_vector * i)
+    block_assignments = {}
+    for area, location in area_assignments.items():
+        for block_location, block_direction in area.pack():
+            
 
-                if possible_location in blockspace.blocks.values():
-                    raise PackingError("Collision with block at location {0}".format(possible_location))
 
-                if blockspace.is_location_out_of_bounds(possible_location):
-                    raise PackingError("Location out of bounds.")
-
-                if compound.isolated and not blockspace.is_isolated(possible_location):
-                    raise PackingError("Location not isolated while isolation is required.")
-
-                if block.block_id not in ISOLATORS:
-                    for isolated_location in blockspace._isolated_blocks_locations:
-                        if isolated_location.is_adjacent(possible_location):
-                            raise PackingError("Location interferes with the isolation of other compounds.")
-
-                assigned_location = possible_location
-
-                yield (block, (assigned_location, current_build_direction))
-
-    if spec_location is None:
-        for location in location_gen(compound, blockspace):
-            try:
-                return dict(_try_pack(location))
-            except PackingError:
-                pass
-        raise PackingError
-    else:
-        return dict(_try_pack(spec_location))
+def pack_areas(areas, blockspace):
+    """
+    Takes a colelction of areas and organizes them inside the
+    :param areas:
+    :param blockspace:
+    :return: dictionery of area and the location inside the blockspace.
+    """
+    pass
