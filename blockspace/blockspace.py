8f5df6af0e8641652e43617618125a863a0a4407
Nicodemes <sasha@paticon.com>
2016-08-17 19:29:55 +0300

    work

--- blockspace/blockspace.py
+++ blockspace/blockspace.py
@@ -15,81 +15,45 @@ class BlockSpace(object):
     def __init__(self, size):
         # Tuple which describes the size of the block space (x,y,z)
         self.size = size
-        # Saves the locations of the blocks.
-        self.blocks = {}
-        self.directions = {}
+
+        self.packed_compounds = {}
+        self.packed_entities = {}
+
         # Saves the locations of the isolated objects
-        self._isolated_blocks_locations = list()
+        self._isolated_blocks_locations = []
         # Saves the locations of the compounds.
-        self.compounds = dict()
+
         # List of units in this blockspace.
-        self.units = []
+        self.unpacked_compounds = []
         # Saves the locations of the entities.
-        self.entities = dict()
+
         # holds to which side each block is faced to.
 
-    def add_unit(self, unit, build_direction=DEF_BUILD_DIRECTION):
+    def add_unit(self, unit):
         """
         takes a unit and tries to place it in the blockspace.
         :param unit: dict of compounds and relative poistion
         """
-        if unit in self.units:
-            return
         for compound in unit.compounds:
-            self.add_compound(compound, build_direction=build_direction)
+            self.add_compound(compound)
 
         for other_unit in unit.dependent_units:
-            self.add_unit(other_unit, build_direction=build_direction)
+            self.add_unit(other_unit)
 
-    def add_compound(self, compound, location=None, build_direction=DEF_BUILD_DIRECTION):
+    def add_compound(self, compound):
         """
-        Mainly, Generate the blocks of that compound to the block dict.
+        Schedule the compound for mapping.
         """
-        if compound in self.compounds:
+        if compound in self.unpacked_compounds:
             return
-        try:
-            assignments = packer.pack(compound, self, location, build_direction)
-            self.add_blocks(assignments)
-            # Save the compound location.
-            # TODO: fix location to the assigned location.
-            self.compounds[compound] = (location, assignments)
-        except packer.PackingError:
-            raise Exception("Can't add compound {0} to this block space.".format(compound))
-
-    def add_compounds(self, compounds, build_direction=DEF_BUILD_DIRECTION):
-        for compound in compounds:
-            self.add_compound(compound, None, build_direction=build_direction)
-
-    def add_blocks(self, blocks, isolated=False):
-        """
-        Add blocks to the block sapce as long as they are not already in it.
-        :param blocks: iterator over blocks to add to the blockspace.
-        :param isolated: if you want the blocks to be isolated.
-        :param build_direction: the direction you want to build the blocks to, if they have the facing option.
-        :return:
-        """
-        for block,  pack_info in blocks.items():
-            coordinate, pack_direction = pack_info
-            if block not in self.blocks:
-                # TODO: think if it is good for here.
-                try:
-                    if block.facing is None:
-                        block.facing = direction.oposite(pack_direction)
-                except AttributeError:
-                    pass
-                self.blocks[block] = coordinate
-                self.directions[block] = pack_direction
-                if isolated:
-                    self._isolated_blocks_locations.append(coordinate)
-
-                block._belongs_to_blockspace = True
+        self.unpacked_compounds.append(compound)
 
     def add_entity(self, entity, location=(0, 0, 0)):
         """
         Add entity to the blockspace.
         """
         location = Vector(*location)
-        self.entities[entity] = location
+        self.packed_entities[entity] = location
 
     # Checkers
     def is_location_out_of_bounds(self, location):
@@ -111,7 +75,7 @@ class BlockSpace(object):
         adjacent_locations = [location + vector for vector in direction.vectors.values()]
 
         # iterate over all the blocks in the blockspace and see if they are adjacent to the location.
-        for block, block_location in self.blocks.items():
+        for block, block_location in self.packed_blocks.items():
             if block_location in adjacent_locations:
                 if block.block_id not in ISOLATORS:
                     return False
@@ -124,9 +88,9 @@ class BlockSpace(object):
         Gets the area of a compound.
         """
 
-        if item in self.compounds:
-            location, blocks = self.compounds[item]
-            block_locations = [self.blocks[block] for block in blocks]
+        if item in self.packed_compounds:
+            location, blocks = self.packed_compounds[item]
+            block_locations = [self.packed_blocks[block] for block in blocks]
 
             # TODO: remove code duplication.
             min_x = sorted(block_locations, key=lambda item: item[0])[0].x
@@ -139,7 +103,7 @@ class BlockSpace(object):
 
             return Vector(min_x, min_y, min_z), Vector(max_x, max_y, max_z)
         else:
-            location = self.blocks[item]
+            location = self.packed_blocks[item]
             return location, location
 
     def get_location_of(self, item):
@@ -147,18 +111,30 @@ class BlockSpace(object):
         Get a location of an item which is stored in the block space.
         """
         try:
-            location = self.blocks[item]
+            location = self.packed_blocks[item]
         except KeyError:
-            location, blocks = self.compounds[item]
+            location, blocks = self.packed_compounds[item]
 
         return location
+
+    def pack(self):
+        compound_packings = packer.pack(self.unpacked_compounds, self)
+        for compound, packing in compound_packings.items():
+            self.packed_compounds[compound] = packing
+
+    @property
+    def packed_blocks(self):
+        to_return = {}
+        for location, blocks in self.packed_compounds.values():
+            to_return.append(blocks)
+        return to_return
 
     def shrink(self):
         """
         Shrinks the blockspace size to the minimal, according to its blocks.
         """
         # TODO: remove code duplication.
-        block_locations = self.blocks.values()
+        block_locations = self.packed_blocks.values()
         max_x = sorted(block_locations, key=lambda item: item[0])[-1].x
         max_y = sorted(block_locations, key=lambda item: item[1])[-1].y
         max_z = sorted(block_locations, key=lambda item: item[2])[-1].z
---
+++ blockspace/mapped_blockspace.py
@@ -0,0 +1,4 @@
+class MappedBlockspace(object):
+    def __init__(self):
+        pass
+    def map_compound(self, blocks, compound):
