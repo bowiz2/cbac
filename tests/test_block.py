8743071302102e5846516507bbd5cd434c6de77c
Nicodemes <sasha@paticon.com>
2016-08-24 21:00:24 +0300

    minor test improve

--- tests/test_block.py
+++ tests/test_block.py
@@ -5,7 +5,9 @@ import cbac
 
 
 class TestBlock(TestCase):
-    def test_block(self):
+    def
+    def test_command_block(self):
         cb = cbac.block.CommandBlock("/say hello", facing=cbac.block.mc_direction.SOUTH, action="chain")
+        self.assertEqual(cb.command, "/say hello")
         redstone = cbac.block.Block(cbac.block.ids.REDSTONE_BLOCK)
         return None
