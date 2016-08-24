05144863e741da4f17a65e07b1bdb5dfd8e1cdda
Nicodemes <sasha@paticon.com>
2016-08-24 22:31:26 +0300

    remove stale imports from or unit

--- sul/or_unit.py
+++ sul/or_unit.py
@@ -1,29 +1,19 @@
-from cbac.compound import CBA
-from cbac.compound import Register
 from cbac.unit.unit_base import Unit
 
 
 class OrUnit(Unit):
     def __init__(self, bits=8):
         # TODO: re-write to new format
-        super(OrUnit, self).__init__()
-        self.bits = bits
-        self.input_a = Register(size=bits)
-        self.input_b = Register(size=bits)
-        self.output = Register(size=bits)
+        super(OrUnit, self).__init__(bits)
+        self.input_a = self.create_input(self.bits)
+        self.input_b = self.create_input(self.bits)
+        self.output = self.create_output(self.bits)
 
-        commands = list()
+        self.synthesis()
 
-        commands.append(self.input_a.shell.reset())
-        commands.append(self.input_b.shell.reset())
-
+    def main_logic_commands(self):
         for a_block, b_block, o_block, in zip(self.input_a.blocks, self.input_b.blocks, self.output.blocks):
-            # note that the eq is overriden.
-            commands.append(a_block.shell == True)
-            commands.append(o_block.shell.activate())
-            commands.append(b_block.shell == True)
-            commands.append(o_block.shell.activate())
-
-        self.cba = CBA(*commands)
-
-        self.compounds = [self.input_a, self.input_b, self.output, self.cba]
+            yield a_block.shell == True
+            yield o_block.shell.activate()
+            yield b_block.shell == True
+            yield o_block.shell.activate()
