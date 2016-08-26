5471491c09a9f64b7c2b197e83cb2cb94849e6ec
Nicodemes <sasha@paticon.com>
2016-08-26 19:41:54 +0300

    Refactor and doc

--- cbac/compound/cba.py
+++ cbac/compound/cba.py
@@ -1,4 +1,3 @@
-import block
 from cbac.block import Block, CommandBlock
 from cbac.command_shell import CBAShell
 from cbac.compound import Compound
@@ -15,38 +14,45 @@ class CBA(Compound):
     def __init__(self, *commands):
         self.cba_id = self.created_count
         self.created_count += 1
-        self.name = "CBA_n{0}".format(self.cba_id)
 
         # commands
         self.user_command_blocks = list(self._gen_cb_chain(commands))
 
         # An empty block which when activated. will fire up this cba.
         self.activator = Block(FALSE_BLOCK)
-        self.system_prefix_blocks = [self.activator]
 
         # This block responsible for deactivating the activator block.
         self.cb_re_setter = CommandBlock(self.activator.shell.deactivate(), None, "chain", True)
         # This command block is reserved for callback use.
         self.cb_callback_reserved = CommandBlock("", None, "chain", True)
 
-        self.system_postfix_blocks = [self.cb_callback_reserved, self.cb_re_setter]
-
         blocks = self.system_prefix_blocks + self.user_command_blocks + self.system_postfix_blocks
 
+        blocks = self.set_block_names(blocks)
+
+        blocks = self.bind_conditions(blocks)
+
+        super(CBA, self).__init__(blocks, isolated=True)
+
+    def set_block_names(self, blocks):
+        """
+        Iterate over blocks and given them names according to their position in the array.
+        :param blocks: blocks which you want ot name.
+        :return: blocks
+        """
         for i, block in enumerate(blocks):
-            # create custom names for blocks.
-            # TODO: make more readable.
+            if hasattr(block, "custom_name"):
+                block.custom_name = "{cba_name}[{i}]".format(cba_name=self.name, i=i)
+        return blocks
 
-            # Deprecated.
-            block.custom_name = "{cba_name}[{i}]".format(cba_name=self.name, i=i)
-            try:
-                action = "<{0}>".format(block.command.command_function.__name__)
-                block.custom_name += action
-            except AttributeError:
-                pass
-            # end of deprecated.
-
-            # Parse conditionaning.
+    @staticmethod
+    def bind_conditions(blocks):
+        """
+        for each condition creating block, set the next block as conditional.
+        :param blocks: list of blocks.
+        :return: blocks.
+        """
+        for i, block in enumerate(blocks):
             try:
                 if block.command.creates_condition:
                     # make the next block conditional.
@@ -56,11 +62,16 @@ class CBA(Compound):
                 pass  # if there is not next block.
             except AttributeError:
                 pass  # in-case this was not a command block or the next block had no conditional option.
-
-        super(CBA, self).__init__(blocks, isolated=True)
+        return blocks
 
     @staticmethod
     def _gen_cb_chain(commands):
+        """
+        Wrappes commands in command blocks, and according to their position decide If they need to be impulse command
+            blocks or chain command blocks.
+        :param commands: a list of commands.
+        :return: CommandBlock generator.
+        """
         assert len(commands) > 0
 
         yield CommandBlock(commands[0], facing=None, action="impulse")
@@ -72,6 +83,18 @@ class CBA(Compound):
     @memoize
     def shell(self):
         return CBAShell(self)
+
+    @property
+    def system_prefix_blocks(self):
+        return [self.activator]
+
+    @property
+    def system_postfix_blocks(self):
+        return [self.cb_callback_reserved, self.cb_re_setter]
+
+    @property
+    def name(self):
+        return "CBA_n{0}".format(self.cba_id)
 
     def __str__(self):
         return self.name
