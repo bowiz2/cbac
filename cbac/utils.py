03872a3c00d9d6118f45a0a8bfb175592b68b1ee
Nicodemes <sasha@paticon.com>
2016-08-27 19:33:17 +0300

    Add the yield from feature

--- cbac/utils.py
+++ cbac/utils.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import types
 
 
 class Vector(namedtuple('Vector', ['x', 'y', 'z'])):
@@ -39,3 +40,35 @@ def format_location(location):
 
 def format_realtive_location(location):
     return " ".join(["~" + str(i) for i in location])
+
+
+def inline_generators(fn):
+    """
+    Used for implementing yield from in python.
+    :param fn:
+    :return:
+    """
+    def inline(value):
+        if isinstance(value, InlineGenerator):
+            for x in value.wrapped:
+                for y in inline(x):
+                    yield y
+        else:
+            yield value
+
+    def wrapped(*args, **kwargs):
+        result = fn(*args, **kwargs)
+        if isinstance(result, types.GeneratorType):
+            result = inline(_from(result))
+        return result
+    return wrapped
+
+
+class InlineGenerator(object):
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+
+
+def _from(value):
+    assert isinstance(value, types.GeneratorType)
+    return InlineGenerator(value)
