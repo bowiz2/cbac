b3af74c1c63367d409a215a7ad9e08654714b652
Nicodemes <sasha@paticon.com>
2016-09-11 20:39:18 +0300

    back_refactor

--- cbac/mc_command.py
+++ cbac/mc_command.py
@@ -1,6 +1,7 @@
 """
 In this module there is an abstract representation of minecraft commands.
 """
+# TODO: refactor creates condition to is_condition_creating. for now we use this because of a back comparability.
 
 
 class MCCommand(object):
@@ -8,16 +9,16 @@ class MCCommand(object):
     Represents a lazy initialized command.
     To use this command, you need to call the compile function.
     """
-    def __init__(self, is_conditional=False, is_creates_condition=False):
+    def __init__(self, is_conditional=False, creates_condition=False):
         """
         :param is_conditional:  If the command is conditional, meaning it will execute only if the
         previously executed command was executed successfully.
-        :param is_creates_condition: If the command creates condition for other commands. Meaning this command will
+        :param creates_condition: If the command creates condition for other commands. Meaning this command will
         force the next command inline to be conditional.
         This is mainly used for testforblock commands.
         """
         self.is_conditional = is_conditional
-        self.is_creates_condition = is_creates_condition
+        self.is_creates_condition = creates_condition
 
     def compile(self):
         """
@@ -32,8 +33,8 @@ class SimpleCommand(MCCommand):
     """
     Represents a simple command which is not relied on some compile-time parameters.
     """
-    def __init__(self, body, is_conditional=False, is_creates_condition=False):
-        super(SimpleCommand, self).__init__(is_conditional, is_creates_condition)
+    def __init__(self, body, is_conditional=False, creates_condition=False):
+        super(SimpleCommand, self).__init__(is_conditional, creates_condition)
         self._body = body
 
     def compile(self):
@@ -49,9 +50,9 @@ class LazyCommand(MCCommand):
     This is a command which is lazy initialized and the uses a function which will be called with the supplied arguments
     in the compilation process.
     """
-    def __init__(self, func, is_conditional=False, is_creates_condition=False, *args, **kwargs):
-        assert all([isinstance(item, bool) for item in [is_conditional, is_creates_condition]]), "must be bool"
-        super(LazyCommand, self).__init__(is_conditional, is_creates_condition)
+    def __init__(self, func, is_conditional=False, creates_condition=False, *args, **kwargs):
+        assert all([isinstance(item, bool) for item in [is_conditional, creates_condition]]), "must be bool"
+        super(LazyCommand, self).__init__(is_conditional, creates_condition)
         self.args = args
         self.kwargs = kwargs
         self.func = func
