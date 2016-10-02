ae23e5df77e1109d0952c702975d5003210f74dc
Nicodemes <sasha@paticon.com>
2016-10-02 14:43:12 +0300

    fix area of affect

--- cbac/entity/pivot.py
+++ cbac/entity/pivot.py
@@ -6,10 +6,10 @@ class Pivot(Entity):
     """
     An armor stand which can move in space and copy areas
     """
-    def __init__(self, area_of_effect, invisible=False):
+    def __init__(self, area_of_affect=((0, 0, 0), (1, 1, 1)), invisible=False):
         """
-        :param area_of_effect: This is the area which is copied to and from the pivot.
+        :param area_of_affect: This is the area which is copied to and from the pivot.
         """
         tags = {"Invisible": 1} if invisible else {}
         super(Pivot, self).__init__(cbac.constants.entity_id.ARMOR_STAND, no_gravity=True, tags=tags)
-        self.area_of_effect = area_of_effect
+        self.area_of_affect = area_of_affect
