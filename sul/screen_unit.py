9ee0581d87e10abbe38d211ae2c3dc98e6f2cdd9
Nicodemes <sasha@paticon.com>
2016-10-02 18:17:00 +0300

    implement fully the screen access unit

--- sul/screen_unit.py
+++ sul/screen_unit.py
@@ -2,13 +2,12 @@ from cbac.unit.unit_base import Unit
 from pymclevel import MCSchematic
 from cbac.unit.statements import *
 from sul import MemoryAccessUnit
-import math
 # TODO: explain what is a char set schematic
 # TODO: support vertical screens.
 
 
 class ScreenUnit(Unit):
-    def __init__(self, char_set, memory_access_unit):
+    def __init__(self, char_set, screen_access_unit):
         """
         :param char_set: Path to a char-set schematic
         :param memory_access_unit: we treat the screen as a memory.
@@ -22,16 +21,19 @@ class ScreenUnit(Unit):
         # 2 dimensional area which is taken by a character.
         self.character_sprite_area = (char_set.size[0], char_set.size[2])
 
-        self.char_set_access_unit = self.add_unit(MemoryAccessUnit())
-        self.screen_access_unit = self.add_unit(memory_access_unit)
+        # This is the unit which will give us the way to navigate in the charecters.
+        self.char_set_access_unit = self.add_unit(
+            MemoryAccessUnit((1, self.char_set_size, 1), self.character_sprite_area)
+        )
+        self.print_value_input = self.add_input(self.char_set_access_unit.address_input)
+        self.screen_access_unit = self.add_unit(screen_access_unit)
         self.location_input = self.add_input(self.screen_access_unit.address_input)
         # TODO: think of a better name.
         # What is the order over the char you want to print
-        self.print_value_input = self.create_input(self.bits)
-
         self.synthesis()
 
     def main_logic_commands(self):
-        # == Here you declare the commands wof the main logic. each command must be yielded out.
+        yield InlineCall(self.char_set_access_unit)
         yield InlineCall(self.screen_access_unit)
-
+        yield self.char_set_access_unit.pivot.shell.store_to_temp(self.character_sprite_area)
+        yield self.screen_access_unit.pivot.shell.load_from_temp(self.character_sprite_area)
