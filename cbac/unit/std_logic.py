8c524f887835388cdb3248cb3f81ef58461e9e05
Nicodemes <sasha@paticon.com>
2016-10-03 23:58:18 +0300

    doc

--- cbac/unit/std_logic.py
+++ cbac/unit/std_logic.py
@@ -4,29 +4,51 @@ import cbac.compound.register
 
 
 class StdLogic(object):
+    """
+    Every std logic item is derived from this class.
+    """
     pass
 
 
 class Port(Block, StdLogic):
+    """
+    Represents a block.
+    """
     def __init__(self):
         super(Port, self).__init__(FALSE_BLOCK)
 
 
 class In(Port):
+    """
+    Ports which are used as input.
+    """
     pass
 
 
 class Out(Port):
+    """
+    Ports which are used as output.
+    """
     pass
 
 
 class Register(cbac.compound.register.Register, StdLogic):
+    """
+    Register inside the unit.
+    soon all the registers must be of this type.
+    """
     pass
 
 
 class InputRegister(Register):
+    """
+    Register which is an input, used in a call statement.
+    """
     pass
 
 
 class OutputRegister(Register):
+    """
+    Register which is an output.
+    """
     pass
