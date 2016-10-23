ab5b29baacf5fe899ff86d8eb7d971575f43ee57
Nicodemes <sasha@paticon.com>
2016-10-23 04:10:13 +0300

    doc

--- cbac/core/compound/__init__.py
+++ cbac/core/compound/__init__.py
@@ -1,3 +1,7 @@
+"""
+Holds all the compounds which are derived from the Compound class, including the class itself.
+Compounds are objects which consist of an array of blocks.
+"""
 from .compound_base import Compound
 from .cba import CBA
 from .constant import Constant
