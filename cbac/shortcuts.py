95b49a20e57cac466c5fd4f0ca1be1ddcf4829b9
Nicodemes <sasha@paticon.com>
2016-11-19 16:50:00 +0200

    create the procedure shortcut

--- cbac/shortcuts.py
+++ cbac/shortcuts.py
@@ -8,4 +8,14 @@ std_unit_gates = [cbac.std_unit.AndGate, cbac.std_unit.OrGate, cbac.std_unit.Nan
 std_unit_arithmetics = [cbac.std_unit.RippleCarryFullAdderArray, cbac.std_unit.IncrementUnit, cbac.std_unit.NegateUnit,
                         cbac.std_unit.SubtractUnit, cbac.std_unit.MultiUnit]
 
+
+class procedure:
+    def __init__(self, name):
+        self.name = name
+
+    def body(self):
+        from cbac.core.compound.cba import CBA
+        from cbac.core.mc_command import say
+        return CBA(say("Procedure: ".format(self.name)))
+
 __all__ = ['std_unit_gates', 'std_unit_arithmetics', 'block_ids']
